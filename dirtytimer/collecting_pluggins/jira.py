a60e00e51cfee1a9eb2eee161243c25f0768be5b
Maksym Shalenyi <enkidulan@gmail.com>
2016-03-14 02:00:03 +0200

    update jira activity collector

--- dirtytimer/collecting_pluggins/jira.py
+++ dirtytimer/collecting_pluggins/jira.py
@@ -24,10 +24,20 @@ class JiraCollector():
         """
         jira = JIRA(
             params['server'], basic_auth=(params['uname'], params['pswd']))
-        # XXX: hadle non-active tasks for period
-        issues = jira.search_issues(
-            'project={project} and assignee = "{user}"'
-            ' and updated < "{before}" AND updated > "{after}"'.format(**params))
+
+        # check issues assigned on user and is in targeted statuses, or status was chened in given period
+
+        # TODO: dates seems to be random...
+        statuses = ("Code Review", "In Progress")
+        subquery_item = '(status CHANGED during ("{after}", "{before}") TO "{status}" and assignee = "{user}")'
+        subquery = " or ".join(subquery_item.format(status=s, **params) for s in statuses)
+        query = '''
+            project={project}
+            and (assignee = "{user}" and status in {statuses})
+            or {subquery}'''.format(subquery=subquery, statuses=statuses, **params)
+        issues = jira.search_issues(query, expand='changelog')
+
+        # import pdb; pdb.set_trace()
 
         work_records = [
             WorkRecord(
